2fc187bcbea18f10df03d078b554ba9793586438
romuald <romualdlevent@gmail.com>
2021-01-25 22:22:27 +0530

    new change and database connexion with cpanel

--- core/database/connection.php
+++ core/database/connection.php
@@ -1,12 +1,12 @@
-<?php 
-	$dsn = 'mysql:host=localhost; dbname=tweety';
-	$user = 'root';
-	$password = '';
- 
+<?php
+	$dsn = 'mysql:host=sql310.epizy.com; dbname=epiz_27770824_tweety';
+	$user = 'epiz_27770824';
+	$password = 'ScD0cDLrArT';
+
 
 	try{
 		$pdo = new PDO($dsn, $user, $password);
 	}catch(PDOException $e){
 		echo 'connection error! ' . $e;
-	}	
+	}
 ?>
